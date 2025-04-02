e43424b25bee66ea006092395b2d83330543def0
Aysun Naghizada <aysunin-bb104@code.edu.az>
2025-04-03 02:38:36 +0400

    CodeEmail

---
+++ Polimorphism/Polimorphism/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorphism
+{
+    class Program
+    {
+        static void Main()
+        {
+            Student student = new Student("Ayla", "Azade");
+            Console.WriteLine($"Email:{student .CodeEmail }");
+        }
+    }
+}
---
+++ Polimorphism/Polimorphism/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorphism
+{
+    public class Student :ICodeAcademy 
+    {
+        private static int count = 0;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CodeEmail { get; set; }
+
+        public Student( string name, string surname) 
+        {
+            Name = name;
+            Surname = surname;
+            count++;
+            Id = count;
+            GenerateEmail();
+        }
+        public void GenerateEmail()
+        {
+            string lastEmail = ($"{Name.ToLower()}.{Surname.ToLower()}{Id}@code.edu.az");
+           CodeEmail = lastEmail;
+        }
+    }
+}
